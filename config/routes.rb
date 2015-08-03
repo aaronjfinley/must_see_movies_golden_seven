9ca885c0f50e7450226708cb2aeba63e77487802
aaronjfinley <aaronjfinley@gmail.com>
2015-08-02 19:57:32 -0500

    director finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
 
+
+
+  # Create
+  get("/directors/new", { :controller => "director", :action => "new_form" })
+  get("/create_director", { :controller => "director", :action => "create_row" })
+
+  # Read
   get("/directors", { :controller => "director", :action => "index" })
-
-  get("/directors/new", { :controller => "director", :action => "new" })
-
   get("/directors/:id", { :controller => "director", :action => "show" })
+
+  # Update
+  get("/directors/:id/edit", { :controller => "director", :action => "edit_form" })
+  get("/update_director/:id", { :controller => "director", :action => "update_row" })
+
+
+  # Delete
+
+  get("/delete_director/:id", { :controller => "director", :action => "destroy" })
+
+
 
 end
